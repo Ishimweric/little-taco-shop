0936f59696696fc8a10e2ea8dcd2971a840eb053
Ishimwe Eric <ishimwee299@gmail.com>
2025-06-18 11:33:40 +0200

    Added a self updating copyright date in the footer with javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const year = document.getElementById('year');
-const thisYear = new Date().getFullYear;
+const thisYear = new Date().getFullYear();
+console.log(thisYear);
 year.setAttribute('datetime', thisYear);
 year.textContent = thisYear;
